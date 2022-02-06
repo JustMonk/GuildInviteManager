427b68072224c11fdc0f57cedefba752a575ef6e
JustMonk <night.fire@mail.ru>
2022-02-06 18:53:53 +0500

    added blacklist

--- Gim.lua
+++ Gim.lua
@@ -31,11 +31,11 @@ function Addon_OnLoad(self, event, ...)
     self:RegisterEvent("PLAYER_LOGIN");
 
     self:RegisterEvent("WHO_LIST_UPDATE");
+    self:RegisterEvent("VARIABLES_LOADED");
 
     self:RegisterForDrag("LeftButton");
 
     print('Gim: AddOn loaded');
-    -- createPlayerTable();
 end
 
 function Addon_OnEvent(self, event, ...)
@@ -48,10 +48,27 @@ function Addon_OnEvent(self, event, ...)
         self:UnregisterEvent("PLAYER_LOGIN");
         print('PLAYER_LOGIN EVENT');
         createPlayerTable();
+        createStatistics();
     elseif event == "WHO_LIST_UPDATE" then
         numResults, totalCount = GetNumWhoResults();
         print("Gim: WHO_LIST_UPDATE EVENT FIRED. numResults: ", numResults, ", totalCount: ", totalCount);
         CurrentWhoResults = numResults;
+    elseif event == "VARIABLES_LOADED" then
+        -- все сохраненные переменные загружены
+        print('variables has been loaded');
+        if HaveWeMetCount == nil then
+            HaveWeMetCount = 0;
+        else
+            HaveWeMetCount = HaveWeMetCount + 1;
+        end
+
+        if PlayerBlacklist == nil then
+            PlayerBlacklist = {};
+        end
+        print('HaveWeMetCount = ', HaveWeMetCount);
+
+        -- test saved variables
+        -- drawStatistics();
     end
 end
 
@@ -101,6 +118,8 @@ function ginvite(name)
     local command = "/ginvite " .. name;
     DEFAULT_CHAT_FRAME.editBox:SetText(command);
     ChatEdit_SendText(DEFAULT_CHAT_FRAME.editBox, 0);
+
+    PlayerBlacklist[name] = 1;
 end
 
 function updateWho()
@@ -108,16 +127,62 @@ function updateWho()
     SendWho("80-80");
 end
 
+-- utils
+function filterList(list, predicate)
+    -- Create the list y containing the items from the list x that satisfy the predicate p. Respect the original ordering. Don't modify x in-place.
+    local filtered = {}
+    for _, v in ipairs(list) do
+        if predicate(v) then
+            filtered[#filtered + 1] = v
+        end
+    end
+end
+
+function get_keys(t)
+    local keys = {}
+    for key, _ in pairs(t) do
+        table.insert(keys, key)
+    end
+    return keys
+end
+-- utils end
+
+-- stats interface
+function createStatistics()
+    local f = StatsBlock;
+    f.blacklist = f:CreateFontString("$parentUsername", "OVERLAY", "GameFontWhiteSmall");
+    f.blacklist:SetText("Blacklist: " .. 0)
+    f.blacklist:SetPoint("TOP", f, 0, -30)
+    f.blacklist:SetPoint("LEFT", f, 5, 0)
+
+    f.invitesCount = f:CreateFontString("$parentHavemeet", "OVERLAY", "GameFontWhiteSmall");
+    f.invitesCount:SetText("Invited: " .. 0)
+    f.invitesCount:SetPoint("TOP", f, 0, -45)
+    f.invitesCount:SetPoint("LEFT", f, 5, 0)
+
+    print('statistics was created');
+    drawStatistics();
+end
+
+function drawStatistics()
+    local f = StatsBlock;
+
+    local keys = get_keys(PlayerBlacklist);
+    local blacklistCount = table.getn(keys);
+
+    --StatsBlockUsername:SetText('sdakjdsakdasd') //можно еще по имени обращаться
+    f.blacklist:SetText("Blacklist: " .. blacklistCount)
+    f.invitesCount:SetText("Invited: " .. blacklistCount)
+
+    print('drawStatistics ended');
+end
+
 -- INIT FUNCTIONs
 function createPlayerRow(name, parent, playername)
-    print(">>> CREATE PLAYER ROW INIT <<<")
-
     local f = CreateFrame("Frame", name, parent);
     f:SetSize(350, 25);
     f:SetPoint("TOP", 0, 0);
     f:SetPoint("LEFT", 0, 0);
-
-    print('frame created');
 
     f.invButton = CreateFrame("Button", "$parentButton", f, "UIPanelButtonTemplate");
     f.invButton:SetText("Invite");
@@ -125,7 +190,6 @@ function createPlayerRow(name, parent, playername)
     f.invButton:SetPoint("CENTER")
     f.invButton:SetPoint("RIGHT", -10, 0);
     -- b:SetFontObject("GameFontNormalSmall")
-    print('button created');
 
     f.username = f:CreateFontString("$parentUsername", "OVERLAY", "GameFontNormalSmall");
     f.username:SetText(playername)
@@ -153,12 +217,10 @@ function createPlayerRow(name, parent, playername)
     -- AddonFS:SetPoint("CENTER",f,"LEFT",0,0) --обязательно должен быть родитель и позиция
     -- AddonFS:SetPoint("CENTER", f)
     -- AddonFS:SetPoint("LEFT", f, 0, 0)
-    print('fontString created');
 
     f.texture = f:CreateTexture();
     f.texture:SetAllPoints(f);
     f.texture:SetTexture(0.5, 0.5, 0.5, 0.3);
-    print('texture created');
 
     return f;
 
@@ -207,7 +269,6 @@ function createPlayerTable()
     for i = 1, 50 do
         -- +25 y offset
         local row = createPlayerRow('row' .. i, ScrollContainer, 'Restmoon' .. i, i);
-        print(row);
 
         row:SetPoint("TOP", 0, (i - 1) * -25);
         PlayerRows[i] = row;
@@ -243,8 +304,32 @@ function updateDatagrid()
 end
 
 function testFrame()
-    createPlayerRow('test2', ScrollContainer)
+    print('test func init');
+
+    local keys = get_keys(PlayerBlacklist);
+
+    for i = 1, table.getn(keys) do
+        print(keys[i]);
+    end
+
+    print('PlayerBlacklist: ', keys);
+    -- createPlayerRow('test2', ScrollContainer)
     -- HideUIPanel(ScrollContainer);
+end
+
+function blacklistToggle()
+    blacklistString = '';
+    local keys = get_keys(PlayerBlacklist);
+    for i = 1, table.getn(keys) do
+        blacklistString = blacklistString .. ', ' .. keys[i]
+    end
+    TestEditBox:SetText(blacklistString)
+
+    if (BlacklistFrame:IsVisible()) then
+        HideUIPanel(BlacklistFrame);
+    else
+        ShowUIPanel(BlacklistFrame)
+    end
 end
 
 function old_scroll_load_function()
